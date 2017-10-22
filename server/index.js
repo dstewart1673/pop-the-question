b29cfb0ca2976c70322f0e9227aa1923a7b78d36
DStewart1673 <DStewart1673@gmail.com>
2017-10-22 10:21:11 -0700

    fixed missing bracket issue

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ passport.deserializeUser((id, done) => {
   mongodb.connect(mongoUrl, (err, db) => {
     const users = db.collection('users');
     users.findOne({
-      id: id
+      id: id,
     }, (err, user) => {
       done(err, user);
       db.close();
@@ -36,7 +36,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  passReqToCallback: true
+  passReqToCallback: true,
 }, (req, accessToken, refreshToken, profile, done) => {
   process.nextTick(() => {
     const prof = parsed(profile);
@@ -46,17 +46,17 @@ passport.use(new GitHubStrategy({
           throw err;
         const users = db.collection('users');
         users.findOne({
-          id: prof.id
+          id: prof.id,
         }, (err, result) => {
           if (err)
             throw err;
           if (!result) {
-            console.log("bleep" + prof.displayName);
+            console.log('bleep' + prof.displayName);
             const newUser = {
-              "id": prof.id,
-              "name": prof.displayName,
-              "polls": []
-            }
+              id: prof.id,
+              name: prof.displayName,
+              polls: [],
+            };
             users.insertOne(newUser, (err, res) => {
               return done(null, parsed(newUser));
               db.close();
@@ -70,6 +70,7 @@ passport.use(new GitHubStrategy({
     } else {
       return done(null, req.user);
     };
+
     function parsed(obj) {
       return obj;
     };
@@ -77,16 +78,18 @@ passport.use(new GitHubStrategy({
 }));
 
 app.use(partials());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(session({secret: process.env.SESSION_SECRET}));
+app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-//Redirects user to GitHub's authentication page.  Once logged in, client will be redirected to their user page.
+//Redirects user to GitHub's authentication page.
+//Once logged in, client will be redirected to their user page.
 app.get('/login', passport.authenticate('github'), (req, res) => {
   //client is sent to GitHub, so this is not used.
 });
@@ -99,7 +102,7 @@ app.get('/api/logout', (req, res) => {
 //TODO:  Make this not create an infinite failure loop.
 app.get('/auth/callback', passport.authenticate('github', {
   successRedirect: '/',
-  failureRedirect: '/login'
+  failureRedirect: '/login',
 }));
 
 //Provides logged-in user's created poll data.
@@ -109,7 +112,7 @@ app.get('/api/user', ensureAuthenticated, (req, res) => {
       throw err;
     const users = db.collection('users');
     users.findOne({
-      id: req.user.id
+      id: req.user.id,
     }, (err, result) => {
       if (err)
         throw err;
@@ -142,10 +145,9 @@ app.get('/api/poll/:pollID', (req, res) => {
       throw err;
     const polls = db.collection('polls');
     polls.findOne({
-      _id: req.params.pollID
+      _id: req.params.pollID,
     }, (err, result) => {
-      if (err)
-        throw err;
+      if (err) throw err;
       res.send(result);
       db.close();
     });
@@ -154,67 +156,66 @@ app.get('/api/poll/:pollID', (req, res) => {
 
 app.get('/api/addpoll', ensureAuthenticated, (req, res) => {
   const newPoll = {
-    "_id": new ObjectID(),
-    "creator": req.user.name,
-    "title": req.query.title,
-    "options": req.query.options
-  }
+    _id: new ObjectID(),
+    creator: req.user.name,
+    title: req.query.title,
+    options: req.query.options,
+  };
   mongodb.connect(mongoUrl, (err, db) => {
-    if (err)
-      throw err;
+    if (err) throw err;
     const users = db.collection('users');
     users.update({
-      id: req.query.id
+      id: req.query.id,
     }, {
       $push: {
-        polls: [newPoll._id, newPoll.title]
-      }
+        polls: [newPoll._id, newPoll.title],
+      },
     }, (err, result) => {
       const polls = db.collection('polls');
       polls.insertOne(newPoll, (err, result) => {
-        if (err)
-          throw err;
+        if (err) throw err;
         res.redirect('/poll/' + newPoll.id.toString());
         db.close();
       });
     });
   });
+});
 
-  app.get('/api/addOpt', ensureAuthenticated, (req, res) => {
-    mongodb.connect(mongoUrl, (err, db) => {
+app.get('/api/addOpt', ensureAuthenticated, (req, res) => {
+  mongodb.connect(mongoUrl, (err, db) => {
+    if (err) throw err;
+    const polls = db.collection('polls');
+    polls.update({
+      id: req.query.id,
+    }, {
+      $push: {
+        options: {
+          option: req.query.option,
+          selections: 0,
+        },
+      },
+    }, (err, result) => {
       if (err)
         throw err;
-      const polls = db.collection('polls');
-      polls.update({
-        id: req.query.id
-      }, {
-        $push: {
-          options: {
-            "option": req.query.option,
-            "selections": 0
-          }
-        }
-      }, (err, result) => {
-        if (err)
-          throw err;
-        res.redirect('/poll/' + req.query.id);
-        db.close();
-      });
+      res.redirect('/poll/' + req.query.id);
+      db.close();
     });
   });
+});
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
+// All remaining requests return the React app, so it can handle routing.
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
-  app.listen(PORT, function() {
-    console.log(`Listening on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    };
-    res.redirect(403, '/')
-  }
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  };
+
+  res.redirect(403, '/');
+}
