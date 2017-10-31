6596b4bcfc7a0154346bd94acb9bfe847c4e6ba3
DStewart1673 <DStewart1673@gmail.com>
2017-10-30 19:41:28 -0700

    fixed component not rendering

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2><a href='/login'>login</a></h2>
-          {(this.state.user) ? <LoggedInHeader /> : <h1>Hello!</h1>}
+          {(this.state.user) ? <LoggedInHeader user={this.state.user} /> : <h1>Hello!</h1>}
           <h2><Link to='/new'>NEW</Link></h2>
         </div>
         <Switch>
