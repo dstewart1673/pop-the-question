3773536933425749e2dbc68c01a046085951951a
DStewart1673 <DStewart1673@gmail.com>
2017-11-11 11:40:59 -0800

    added import Link to userpage

--- react-ui/src/UserPage.js
+++ react-ui/src/UserPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 //import { Link } from 'react-router-dom';
 import './UserPage.css';
