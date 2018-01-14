d218f6b8e11b2859b62a42a94e2af577a18ce9ae
DStewart1673 <DStewart1673@gmail.com>
2018-01-14 12:13:32 -0800

    stretched navbar

--- react-ui/src/Header.js
+++ react-ui/src/Header.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import LoggedButtons from './LoggedButtons';
 import UnloggedButtons from './UnloggedButtons';
+import './style/Header.css';
 
 class Header extends Component {
   constructor(props) {
