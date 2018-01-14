edb2e227820f7fb5f5932924df41f60dc8fb8692
DStewart1673 <DStewart1673@gmail.com>
2018-01-14 12:39:47 -0800

    test

--- react-ui/src/Header.js
+++ react-ui/src/Header.js
@@ -12,7 +12,7 @@ class Header extends Component {
 
   render() {
     return (
-      <Navbar inverse collapseOnSelect staticTop >
+      <Navbar inverse collapseOnSelect fluid>
         <Navbar.Header>
           <Navbar.Brand>
             <h2>Pop! The Question!</h2>
