d4394beb01e803342aae6a1a49d441c45dd8d3bf
DStewart1673 <DStewart1673@gmail.com>
2018-01-15 15:11:29 -0800

    added formatting to home

--- react-ui/src/Home.js
+++ react-ui/src/Home.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 
 const Home = () => (
-  <Jumbotron>
-    <h1>Hello!</h1>
-    <p>Welcome to Pop! the Question! a simple little polling app made by
-      <a href='dstewart1673.github.io'>Daniel Stewart</a>.
-    </p>
-    <p>
-      <Button bsStyle='primary'>To the polls!</Button>
-    </p>
-  </Jumbotron>
+  <Grid>
+    <Row>
+      <Col xs={12} md={10}>
+        <Jumbotron>
+          <h1>Hello!</h1>
+          <p>Welcome to Pop! the Question! a simple little polling app made by
+            <a href='dstewart1673.github.io'>Daniel Stewart</a>.
+          </p>
+          <p>
+            <Button bsStyle='primary'>To the polls!</Button>
+          </p>
+        </Jumbotron>
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default Home;
