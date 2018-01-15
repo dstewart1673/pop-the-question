dc678b18f564fdb22efd804c8895c9117bf4fdbb
DStewart1673 <DStewart1673@gmail.com>
2018-01-14 17:48:00 -0800

    added missing imports

--- react-ui/src/UserPage.js
+++ react-ui/src/UserPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 //import { Link } from 'react-router-dom';
