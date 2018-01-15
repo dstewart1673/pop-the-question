96453943402ec15bc3a358e81b240632ebc20500
DStewart1673 <DStewart1673@gmail.com>
2018-01-15 11:05:52 -0800

    dbg

--- react-ui/src/Home.js
+++ react-ui/src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Home = () => (
-  <home>
+  <div>
     <h1>Hello!  Welcome to Pop! the Question!</h1>
     <h2>Click <a href="/api/login">here</a> to login, or <a href="/polls">here </a>
      to go straight to the polls!</h2>
-  </home>
+  </div>
 );
 
 export default Home;
