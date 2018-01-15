f51925d04987615d2c2007c40a29d2afe4dce95f
DStewart1673 <DStewart1673@gmail.com>
2018-01-15 15:14:02 -0800

    added grid, row, col

--- react-ui/src/Home.js
+++ react-ui/src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, Button } from 'react-bootstrap';
 
 const Home = () => (
   <Grid>
