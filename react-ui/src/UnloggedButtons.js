d9f1d1fa2456417019b09a667dbc4582255b7ebe
DStewart1673 <DStewart1673@gmail.com>
2018-01-15 11:01:29 -0800

    debug

--- react-ui/src/UnloggedButtons.js
+++ react-ui/src/UnloggedButtons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 
-const UnloggedButtons = () => (
+UnloggedButtons => (
   <Nav navbar pullRight className='navAdjust'>
     <NavItem eventKey={1} href='/login'>
       Login
