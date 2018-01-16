18b52255814f17fdb7de4cc597c5cc261702152b
DStewart1673 <DStewart1673@gmail.com>
2018-01-15 16:27:13 -0800

    removed navbar from footer

--- react-ui/src/Footer.js
+++ react-ui/src/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
 
 const Footer = () => (
-  <Navbar inverse fixedBottom>
-    <Navbar.Text>Disclaimer</Navbar.Text>
-  </Navbar>
+  <div>
+    <h3>Disclaimer</h3>
+  </div>
 );
 
 export default Footer;
